24211d47a3e5b9f8ee09933bdd4b138edc1b1ad0
guru2402 <prasadguru141@gmail.com>
2017-07-20 19:00:31 +0530

    excel output

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 
 var express = require("express"),
 	bodyParser = require("body-parser"),
-	mongoose	= require("mongoose");
+	mongoose	= require("mongoose"),
+    mongoXlsx = require('mongo-xlsx'),
 	ejs          = require("ejs");
+var MongoClient = require('mongodb').MongoClient;
+
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost/attendance");
 var app = express();
@@ -21,10 +24,24 @@ var userSchema = new mongoose.Schema ({
 });
 var users = mongoose.model("users" , userSchema);
 
-app.get("/" , (req,res) => {
-	res.render('new');
+app.get("/spreadsheet" , (req,res) => {   
+    MongoClient.connect("mongodb://localhost/attendance", (err, db)=>{
+        db.collection('users', function (err, collection) {
+            collection.find().toArray(function(err, items) {
+                if(err) throw err;            
+                var model = mongoXlsx.buildDynamicModel(items);
+                mongoXlsx.mongoData2Xlsx(items, model, function(err, data) {
+                  console.log('File saved at:', data.fullPath); 
+                  res.render("new",{error:"Excel generated sucessfully in the name : " + data.fullPath})
+                });
+            })
+        })
+    })
 });
 
+app.get("/",(req,res)=>{
+    res.render("new");
+})
 app.post("/" , (req , res) =>{
 	 var Users = new users({
 		 name : req.body.name,
@@ -35,14 +52,10 @@ app.post("/" , (req , res) =>{
 	 })
 	 Users.save().then((doc)=>{
 		 console.log(doc)
-		 res.send("<script>alert('Your attendance is marked')</script>");
+		 res.render("new",{error:"Attendance marked sucessfully"});
 	 },(err)=>{
 		 console.log("Error: ",err);
 	 })
-
-
-
-
 })
 
 
